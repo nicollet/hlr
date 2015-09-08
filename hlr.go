c25f42825709c583ecc79188c42aebbeaae587ad
Xavier Nicollet <xnicollet@gmail.com>
2015-09-08 09:41:50 +0200

    Playing with generators.

--- hlr.go
+++ hlr.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"bufio"
-	//"fmt"
+	"errors"
+	"fmt"
 	"log"
 	"os"
 	"regexp"
@@ -12,10 +13,63 @@ import (
 func main() {
 	// include_regexp, _ := regexp.Compile(`^\s*include\s+"(.*?)"`)
 	rf := newReadFile()
-	rf.readFile("test")
-	//for _, line := range rf.readFile("test") {
-	//	fmt.Println(line)
+	lines := rf.readFile("test")
+	//for filePos := range getRunes(lines) {
+	//fmt.Printf("line: %d, offset: %d, %c \n",
+	//	filePos.lineNumber, filePos.lineOffset, filePos.r)
+	//	fmt.Printf("%c", filePos.r)
 	//}
+	chFilePos := getRunes(lines)
+	for {
+		filePos, err := nextNoSpace(chFilePos)
+		if err != nil {
+			fmt.Print(err)
+		} else {
+			fmt.Printf("%c", filePos.r)
+		}
+	}
+}
+
+func nextNoSpace(ch chan FilePos) (FilePos, error) {
+	var filePos FilePos
+	for {
+		filePos = <-ch
+		r := filePos.r
+		if r != ' ' || r != '\t' || r != '\n' {
+			return filePos, nil
+		}
+		fmt.Printf("%c", filePos.r)
+	}
+	return filePos, errors.New("Can't find any space")
+}
+
+type FilePos struct {
+	r          rune // current rune
+	lineNumber int
+	lineOffset int // offset on current line
+}
+
+// generator to advance one rune by one
+
+// generator to get rune by rune a []string
+func getRunes(lines []string) chan FilePos {
+	ch := make(chan FilePos)
+	var filePos FilePos
+	go func() {
+		for ln, line := range lines {
+			filePos.lineNumber = ln
+			for offset, r := range line {
+				filePos.lineOffset = offset
+				filePos.r = r
+				ch <- filePos
+			}
+			filePos.lineOffset += 1
+			filePos.r = '\n'
+			ch <- filePos
+		}
+		close(ch)
+	}()
+	return ch
 }
 
 type ReadFile struct {
@@ -97,89 +151,11 @@ func (rf ReadFile) readOneFile(file_name string, lines []string) []string {
 //	rules map[int][]string // or map[int][]*string ?
 //}
 
-type itemType int
-const (
-	itemEqual = iota,
-	itemVariable, // a variable
-	itemString, // value of a variable (with quotes)
-	itemEOF,
-	item.Error, // an error
-)
-
-type item struct {
-	typ itemType
-	val string
-}
-
-// just a handy print function
-func (i item) String() string {
-	switch i.typ {
-	case item.EOF:
-		return "EOF"
-	case item.Error:
-		return i.val
-	}
-	if len(i.val) > 10 {
-		return fmt.Sprintf("%.10q...", i.val)
-	}
-	return fmt.Sprintf("%q", i.val)
-}
-
-// stateFn represents the state of the scanner
-// as a function that returns the next state
-type stateFn func(*lexer) stateFn
-
-// notice the channel, ignore the rest
-type lexer struct {
-	name string // used only for error report
-	input string // the string being scanned
-	start int // start position of this item
-	pos int // current position in the input
-	width int // width of last rune read from input
-	items chan item // channel of scanned items
-}
-
-// start a lexer
-func lex(name, input string) (*lexer, chan item) {
-	l := &lexer{
-		name: name,
-		input: input,
-		items: make(chan item),
-	}
-	go l.run() // Concurently run state machine
-	return l, l.items
-}
-
-func (l *lexer) run() {
-	for state := lexText; state != nil {
-		state = state(l)
-	}
-	close(l.items) // No more tokens will be delivered
-}
-
-// emit passes an item back to the client
-func (l *lexer) emit(t itemType) {
-	l.items <- item{t, l.input[l.start:l.pos]}
-	l.start = l.pos
-}
-
-func initVariables(lines []string) map[string]string {
-	variables := map[string]string{
-		"RFC1918": "10.0.0.0/8,192.168.0.0/16,172.16.0.0/12",
-	}
-}
-
-func (remote Remote) generate() {
-	tables := make(map[string]string) // retour: type table
-
-	rf := newReadFile()
-	lines := rf.readFile(remote.configFile)
-	if len(s) == 0 {
-		panic(fmt.Sprintf("%s can't be read. Aborting."))
-	}
-
-	// Here build and show variables
-
-}
+//func initVariables(lines []string) map[string]string {
+//	variables := map[string]string{
+//		"RFC1918": "10.0.0.0/8,192.168.0.0/16,172.16.0.0/12",
+//	}
+// var newVar string = ""
+//}
 
 // vim: set sw=2 ts=2 list:
