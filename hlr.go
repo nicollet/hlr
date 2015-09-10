e39dd99f58b78a7a38cac00805cb6b15e3cc9b8f
Xavier Nicollet <xnicollet@gmail.com>
2015-09-10 10:25:51 +0200

    Beginning to look like a state machine.

--- hlr.go
+++ hlr.go
@@ -2,37 +2,56 @@ package main
 
 import (
 	"bufio"
-	"errors"
+	// "errors"
 	"fmt"
 	"log"
 	"os"
 	"regexp"
 	"strings"
+	"unicode/utf8"
 )
 
 func main() {
-	// include_regexp, _ := regexp.Compile(`^\s*include\s+"(.*?)"`)
 	rf := newReadFile()
-	lines := rf.readFile("test")
-	//for filePos := range getRunes(lines) {
-	//fmt.Printf("line: %d, offset: %d, %c \n",
-	//	filePos.lineNumber, filePos.lineOffset, filePos.r)
-	//	fmt.Printf("%c", filePos.r)
-	//}
-	chFilePos := getRunes(lines)
-	for {
-		filePos, err := nextNoSpace(chFilePos)
-		if err != nil {
-			break
-		}
-		fmt.Printf("first letter: %c ", filePos.r)
-		varName, err := getUpWord(chFilePos, filePos.r)
-		if err != nil {
-			fmt.Printf("not Upcase: %v\n", err)
-			continue
-		}
-		fmt.Printf("variable name: %v\n", varName)
+	text := rf.readFile("test")
+
+	l := lexer{
+		input: text,
+		items: make(chan item),
 	}
+
+	go l.run()
+	for i := 0; i < 1000; i++ {
+		fmt.Printf("item: %v\n", <-l.items)
+	}
+
+	//	for line := 0; line < 10; line++ {
+	//		l.nextNoSpace()
+	//		if l.getUpWord() {
+	//			varName := <-l.items
+	//			fmt.Printf("item: %v\n", varName)
+	//
+	//			// looking for =
+	//			l.nextNoSpace()
+	//			if l.getEqual() {
+	//				equal := <-l.items
+	//				fmt.Printf("item: %v\n", equal)
+	//
+	//				l.nextNoSpace()
+	//				if l.getVarValue() {
+	//					value := <-l.items
+	//					fmt.Printf("item: %v\n", value)
+	//				} else {
+	//					fmt.Printf("No variable")
+	//					l.nextLine()
+	//				}
+	//			} else {
+	//				l.nextLine()
+	//			}
+	//		} else {
+	//			l.nextLine()
+	//		}
+	//	}
 }
 
 func isSpace(r rune) bool {
@@ -42,64 +61,206 @@ func isSpace(r rune) bool {
 	return false
 }
 
-func nextNoSpace(ch chan FilePos) (FilePos, error) {
-	for filePos := range ch {
-		r := filePos.r
-		if !isSpace(r) {
-			return filePos, nil
-		}
-		// fmt.Printf("%c", filePos.r)
+func isUpper(r rune) bool {
+	if r >= 'A' && r <= 'Z' {
+		return true
 	}
-	return FilePos{}, errors.New("Can't find any space")
+	return false
 }
 
-func getUpWord(ch chan FilePos, firstLetter rune) (upWord string, Err error) {
-
-	r := firstLetter
-	var vName string = string(r)
-	if !(r == '_' || r >= 'A' && r <= 'Z') {
-		return string(r), errors.New("First letter not uppcase " + string(r))
+func isUpperOrDigit(r rune) bool {
+	if isUpper(r) || r >= '0' && r <= '9' {
+		return true
 	}
-	for filePos := range ch {
-		r = filePos.r
-		if isSpace(r) {
+	return false
+}
+
+type item struct {
+	typ itemType
+	val string
+}
+
+type itemType int
+
+const (
+	itemError itemType = iota
+	itemVarName
+	itemEqual
+	itemVarValue
+	itemEOF
+)
+
+type stateFn func() stateFn
+
+type lexer struct {
+	name      string // to debug
+	input     string // text
+	itemStart int
+	pos       int // current position
+	width     int // width of current rune
+	items     chan item
+}
+
+func (l *lexer) run() {
+	for state := l.getUpWord(); state != nil; {
+		state = state()
+	}
+	close(l.items)
+}
+
+func (l *lexer) next() rune {
+	if l.pos >= len(l.input) {
+		l.width = 0
+		return 'Z'
+	}
+	var r rune
+	r, l.width = utf8.DecodeRuneInString(l.input[l.pos:])
+	l.pos += l.width
+	return r
+}
+
+func (l *lexer) backup() {
+	l.pos -= l.width
+}
+
+func (l *lexer) ignore() {
+	l.itemStart = l.pos
+}
+
+// return the number of spaces encountered
+func (l *lexer) nextNoSpace() (ret int) {
+	for {
+		r := l.next()
+		if r == 'Z' {
 			break
 		}
-		if !(r == '_' || r >= 'A' && r <= 'Z' || r >= '0' && r <= '9') {
-			return "", errors.New("No uppcase word" + string(r))
+		// place the cursor at the beginning of next word
+		if !isSpace(r) {
+			l.backup()
+			break
 		}
-		vName += string(r)
+		ret++
 	}
-	return vName, nil
+	l.ignore()
+	return ret
 }
 
-type FilePos struct {
-	r          rune // current rune
-	lineNumber int
-	lineOffset int // offset on current line
-}
-
-// generator to advance one rune by one
-
-// generator to get rune by rune a []string
-func getRunes(lines []string) chan FilePos {
-	ch := make(chan FilePos)
-	var filePos FilePos
-	go func() {
-		for ln, line := range lines {
-			filePos.lineNumber = ln
-			for offset, r := range line {
-				filePos.lineOffset = offset
-				filePos.r = r
-				ch <- filePos
-			}
-			filePos.lineOffset += 1
-			filePos.r = '\n'
-			ch <- filePos
+func (l *lexer) nextLine() {
+	for {
+		r := l.next()
+		if r == 'Z' {
+			break
 		}
-		close(ch)
-	}()
-	return ch
+		if r == '\n' {
+			l.ignore() // cursor at the beginning of next line
+			break
+		}
+	}
+}
+
+func (l *lexer) emit(t itemType) {
+	l.items <- item{t, l.input[l.itemStart:l.pos]}
+	l.itemStart = l.pos
+}
+
+func (l *lexer) emitString(t itemType, val string) {
+	l.items <- item{t, val}
+	l.itemStart = l.pos
+}
+
+func (l *lexer) getEqual() stateFn {
+	r := l.next()
+	if r == '=' {
+		l.emit(itemEqual)
+		l.nextNoSpace()
+		return l.getVarValue
+	}
+	l.nextLine()
+	return l.getUpWord
+}
+
+// Have we found an upcase word
+func (l *lexer) getUpWord() stateFn {
+	l.nextNoSpace()
+	r := l.next()
+	if r == 'Z' {
+		return nil
+	}
+	if !(isUpper(r) || r == '_') {
+		l.nextLine()
+		return l.getUpWord
+	}
+
+	for {
+		r = l.next()
+		if r == 'Z' {
+			return nil
+		}
+
+		if !(isUpperOrDigit(r) || r == '_') {
+			break
+		}
+	}
+	if isSpace(r) || r == '=' {
+		l.backup()
+		l.emit(itemVarName)
+		l.nextNoSpace()
+		return l.getEqual
+	}
+	l.nextLine()
+	return l.getUpWord
+}
+
+// we are already at the first non space character
+func (l *lexer) getVarValue() stateFn {
+	r := l.next()
+	if r != '"' {
+		fmt.Printf("not first double-quotes: %c\n", r)
+		return nil
+	}
+	l.nextNoSpace()
+
+	var val string
+	successive_spaces := 0
+	for {
+		r = l.next()
+		if r == '"' {
+			// remove last comma if needed
+			if strings.HasSuffix(val, ",") {
+				val = val[:len(val)-1]
+			}
+			l.emitString(itemVarValue, val)
+			l.nextNoSpace()
+			return l.getUpWord
+		}
+		if isSpace(r) {
+			successive_spaces++
+			continue
+		}
+		if successive_spaces > 0 {
+			val += ","
+			successive_spaces = 0
+		}
+		val += string(r)
+	}
+	return nil // never get there: errors to be handled
+}
+
+func (i item) String() string {
+	switch i.typ {
+	case itemEOF:
+		return "EOF"
+	case itemVarName:
+		return fmt.Sprintf("variable: %s", i.val)
+	case itemEqual:
+		return "="
+	case itemVarValue:
+		return fmt.Sprintf("varValue: %s", i.val)
+	}
+	if len(i.val) > 10 {
+		return fmt.Sprintf("%.10q...", i.val)
+	}
+	return fmt.Sprintf("%q", i.val)
 }
 
 type ReadFile struct {
@@ -114,10 +275,10 @@ func newReadFile() ReadFile {
 	}
 }
 
-func (rf ReadFile) readFile(file_name string) []string {
-	var lines []string
+func (rf ReadFile) readFile(file_name string) string {
+	var text string
 	if file_name[0] == '/' {
-		return rf.readOneFile(file_name, lines)
+		return rf.readOneFile(file_name, text)
 	}
 
 	for _, p := range []string{"hosts", "includes"} {
@@ -135,18 +296,18 @@ func (rf ReadFile) readFile(file_name string) []string {
 			if hask {
 				continue
 			}
-			lines_ := rf.readOneFile(file_name, lines)
-			lines = append(lines, lines_...)
+			new_text := rf.readOneFile(file_name, text)
+			text += new_text
 		}
 	}
-	return lines
+	return text
 }
 
 func stripnb(hostname string) string {
 	return strings.Trim(hostname, "0123456789")
 }
 
-func (rf ReadFile) readOneFile(file_name string, lines []string) []string {
+func (rf ReadFile) readOneFile(file_name string, text string) string {
 	rf.doneFiles[file_name] = true
 	file, err := os.Open(file_name)
 	if err != nil {
@@ -154,20 +315,18 @@ func (rf ReadFile) readOneFile(file_name string, lines []string) []string {
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
-	// re := regexp.MustCompile(`^\s*include\s+"(.*?)"`)
 	re := rf.includeRegexp
 	for scanner.Scan() {
 		line := scanner.Text()
 		includes := re.FindSubmatch([]byte(line))
 		if len(includes) >= 1 {
-			// fmt.Printf("! include %s\n", includes[1])
 			include_fname := string("includes/" + string(includes[1]))
-			lines = append(lines, rf.readFile(include_fname)...)
+			text += rf.readFile(include_fname)
 		} else {
-			lines = append(lines, line)
+			text += line + string('\n')
 		}
 	}
-	return lines
+	return text
 }
 
 // Maybe later: easier stuff before
